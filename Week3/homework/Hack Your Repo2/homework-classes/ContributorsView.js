b605c4a3cabe68fb471717d47f0ef9ae2c9708ed
enwer <tewbejan@gmail.com>
2020-02-09 00:32:01 +0100

    js3w3 enwer

--- Week3/homework/Hack Your Repo2/homework-classes/ContributorsView.js
+++ Week3/homework/Hack Your Repo2/homework-classes/ContributorsView.js
@@ -1,19 +1,15 @@
 'use strict';
-
 {
   const { createAndAppend } = window.Util;
-
   class ContributorsView {
     constructor(container) {
       this.container = container;
     }
-
     update(state) {
       if (!state.error) {
         this.render(state.contributors);
       }
     }
-
     /**
      * Renders the list of contributors
      * @param {Object[]} contributors An array of contributor objects
@@ -21,8 +17,18 @@
     render(contributors) {
       // TODO: replace this comment and the console.log with your own code
       console.log('ContributorsView', contributors);
+      while (this.container.hasChildNodes()) {
+        this.container.removeChild(this.container.lastChild);
+      }
+      contributors.forEach(contributor => {
+        (this.div = createAndAppend('div', this.container, {
+          class: 'contriDiv',
+        })),
+          createAndAppend('h5', this.div, { text: contributor.login }),
+          createAndAppend('h4', this.div, { text: contributor.contributions }),
+          createAndAppend('img', this.div, { src: contributor.avatar_url });
+      });
     }
   }
-
   window.ContributorsView = ContributorsView;
 }
--- Week3/homework/Hack Your Repo2/homework-classes/RepoView.js
+++ Week3/homework/Hack Your Repo2/homework-classes/RepoView.js
@@ -1,19 +1,15 @@
 'use strict';
-
 {
   const { createAndAppend } = window.Util;
-
   class RepoView {
     constructor(container) {
       this.container = container;
     }
-
     update(state) {
       if (!state.error) {
         this.render(state.selectedRepo);
       }
     }
-
     /**
      * Renders the repository details.
      * @param {Object} repo A repository object.
@@ -21,8 +17,17 @@
     render(repo) {
       // TODO: replace this comment and the console.log with your own code
       console.log('RepoView', repo);
+      while (this.container.hasChildNodes()) {
+        this.container.removeChild(this.container.lastChild);
+      }
+      this.div = createAndAppend('div', this.container, { class: 'info' });
+      createAndAppend('h5', this.div, { text: 'Repository:' + repo.name });
+      createAndAppend('h5', this.div, {
+        text: 'Description:' + repo.description,
+      });
+      createAndAppend('h5', this.div, { text: 'Forks:' + repo.forks });
+      createAndAppend('h5', this.div, { text: 'Updated:' + repo.updated_at });
     }
   }
-
   window.RepoView = RepoView;
 }
